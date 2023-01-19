7c9c119e0cf5919e43d2638fa3c5f60c0005cc51
Jeff Raubitschek <jeff@wandb.com>
2023-01-18 16:04:58 -0800

    update

--- setup.py
+++ setup.py
@@ -1,28 +1,49 @@
-"""yea setup."""
+"""nexus setup."""
 
-from setuptools import setup
 from distutils.command.install import install
+import os
+from pathlib import Path
+import platform
+import subprocess
+from setuptools import setup
+
+PACKAGE: str = "wandb_nexus"
 
 
 class post_install(install):
-    def __init__(self, dist_obj):
-        super().__init__(dist_obj)
-        print("GOT INIT *************", self, dist_obj)
-        print(vars(self))
-        # assert False
+    def _get_nexus_path(self, goos=None, goarch=None):
+        goos = goos or platform.system().lower()
+        goarch = goarch or platform.machine().lower()
+        base = Path(self.install_platlib) / PACKAGE
+        path = (base / f"bin-{goos}-{goarch}" / "wandb-nexus").resolve()
+        return path
+
+    def _build_nexus(self, goos=None, goarch=None):
+        nexus_path = self._get_nexus_path(goos=goos, goarch=goarch)
+        src_dir = Path(__file__).parent / "nexus"
+        env = {}
+        if goos:
+            env["GOOS"] = goos
+        if goarch:
+            env["GOARCH"] = goarch
+        os.makedirs(nexus_path.parent, exist_ok=True)
+        ldflags = "-s -w"
+        cmd = ("go", "build", f"-ldflags={ldflags}", "-o", str(nexus_path), "cmd/nexus_server/main.go")
+        subprocess.check_call(cmd, cwd=src_dir, env=dict(os.environ, **env))
 
     def run(self):
-        for x in range(40):
-            print("RUN1", x)
         install.run(self)
-        for x in range(40):
-            print("RUN2", x)
+
+        nexus_path = self._get_nexus_path()
+        if not nexus_path.exists():
+            self._build_nexus()
+
 
 setup(
     name="wandb-nexus",
     version="0.0.1.dev1",
     description="Wandb core",
-    packages=["wandb_nexus"],
+    packages=[PACKAGE],
     zip_safe=False,
     include_package_data=True,
     license="MIT license",
