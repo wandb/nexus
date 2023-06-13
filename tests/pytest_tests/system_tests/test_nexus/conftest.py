3596186578649aba8422b1b69624e6e2b8a19c43
Dmitry Duev <dima@wandb.com>
2023-06-13 12:37:12 -0700

    save progress

--- tests/pytest_tests/system_tests/test_nexus/conftest.py
+++ tests/pytest_tests/system_tests/test_nexus/conftest.py
@@ -2,6 +2,6 @@ import pytest
 import wandb
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def wandb_require_nexus():
     wandb.require(experiment="nexus")
--- tests/pytest_tests/system_tests/test_nexus/test_nexus.py
+++ tests/pytest_tests/system_tests/test_nexus/test_nexus.py
@@ -2,6 +2,6 @@ def test_wandb_init(relay_server, wandb_init):
     with relay_server() as relay:
         run = wandb_init()
         run.log({"nexus": 1337})
-        history = relay.context.get_run_history(run.id)
-        assert history[0]["nexus"] == 1337
         run.finish()
+    history = relay.context.get_run_history(run.id)
+    assert history["nexus"][0] == 1337
