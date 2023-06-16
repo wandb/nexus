34c7a9d06e01d4c0ee5704620159fe9763d565ee
Dmitry Duev <dima@wandb.com>
2023-06-16 00:17:33 -0700

    comment stream_mux

--- nexus/pkg/server/stream_mux.go
+++ nexus/pkg/server/stream_mux.go
@@ -4,6 +4,10 @@ import (
 	"sync"
 )
 
+// StreamMux is a multiplexer for streams.
+// It is thread-safe and is used to ensure that
+// only one stream exists for a given streamId so that
+// we can safely add responders to streams.
 type StreamMux struct {
 	mux   map[string]*Stream
 	mutex sync.RWMutex
@@ -15,6 +19,7 @@ func NewStreamMux() *StreamMux {
 	}
 }
 
+// addStream adds a stream to the mux if it doesn't already exist.
 func (sm *StreamMux) addStream(streamId string, settings *Settings) *Stream {
 	sm.mutex.RLock()
 	defer sm.mutex.RUnlock()
@@ -40,6 +45,7 @@ func (sm *StreamMux) getStream(streamId string) (*Stream, bool) {
 //  	delete(sm.mux, streamId)
 // }
 
+// Close closes all streams in the mux.
 func (sm *StreamMux) Close() {
 	sm.mutex.RLock()
 	defer sm.mutex.RUnlock()
@@ -49,8 +55,8 @@ func (sm *StreamMux) Close() {
 		wg.Add(1)
 		go stream.Close(&wg) // test this
 	}
-
 	wg.Wait()
 }
 
+// StreamMux is a singleton.
 var streamMux = NewStreamMux()
