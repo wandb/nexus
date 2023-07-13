234766d92038343a9710ce9b8e12b1ea10626dc3
Dmitry Duev <dima@wandb.com>
2023-07-12 17:36:55 -0700

    clean up

--- nexus/scripts/pre-commit-hooks/run-go-build.sh
+++ nexus/scripts/pre-commit-hooks/run-go-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # From: https://github.com/dnephin/pre-commit-golang/blob/master/run-go-build.sh
 
-
+# shellcheck disable=SC2164
 cd nexus
 FILES=$(go list ./...  | grep -v /vendor/)
 COMMIT=$(git rev-parse HEAD)
