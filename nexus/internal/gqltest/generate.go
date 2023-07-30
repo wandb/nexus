7b1fb895ccf91403188d2cc66fb5f1f82d2acadc
Jeff Raubitschek <jeff@wandb.com>
2023-07-30 08:09:39 -0400

    update

--- nexus/internal/gqltest/generate.go
+++ nexus/internal/gqltest/generate.go
@@ -1,3 +1,3 @@
 package gqltest
 
-//go:generate mockgen -destination mock_client.go -package gqltest github.com/Khan/genqlient/graphql Client
+//go:generate mockgen -destination gqltest_gen.go -package gqltest github.com/Khan/genqlient/graphql Client
