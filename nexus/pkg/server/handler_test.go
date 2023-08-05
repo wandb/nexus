c4a03980ab291afb2901243d86648d0c18ed5062
dmitryduev <dmitry.duev@gmail.com>
2023-08-05 01:35:07 -0700

    test(nexus): add gpt-generated unit tests to improve coverage

--- nexus/pkg/server/handler_test.go
+++ nexus/pkg/server/handler_test.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"context"
+	"github.com/wandb/wandb/nexus/internal/nexuslib"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/wandb/wandb/nexus/pkg/monitor"
@@ -60,4 +63,138 @@ func TestGetRun(t *testing.T) {
 
 	returnedRunRecord := handler.GetRun()
 	assert.Equal(t, runRecord, returnedRunRecord)
+}
+
+func TestHandleRunStart(t *testing.T) {
+	ctx := context.Background()
+	settings := &service.Settings{}
+	logger := &observability.NexusLogger{}
+	handler := NewHandler(ctx, settings, logger)
+
+	startTime := time.Unix(1000000, 0)
+	run := &service.RunRecord{
+		StartTime: timestamppb.New(startTime),
+	}
+	request := &service.RunStartRequest{
+		Run: run,
+	}
+	record := &service.Record{}
+
+	handler.handleRunStart(record, request)
+
+	assert.Equal(t, float64(1000), handler.startTime)
+	assert.Equal(t, run, handler.runRecord)
+}
+
+func TestHandleAttach(t *testing.T) {
+	runRecord := &service.RunRecord{}
+	handler := &Handler{
+		runRecord: runRecord,
+	}
+
+	response := &service.Response{}
+	handler.handleAttach(nil, response)
+
+	assert.Equal(t, &service.AttachResponse{Run: runRecord}, response.GetAttachResponse())
+}
+
+func TestHandleSummary(t *testing.T) {
+	handler := &Handler{
+		recordChan:          make(chan *service.Record, 1),
+		consolidatedSummary: make(map[string]string),
+	}
+
+	update := []*service.SummaryItem{
+		{Key: "key1", ValueJson: "value1"},
+		{Key: "key2", ValueJson: "value2"},
+	}
+	summary := &service.SummaryRecord{Update: update}
+	record := &service.Record{}
+	handler.handleSummary(record, summary)
+
+	summaryRecord := <-handler.recordChan
+	expectedSummary := nexuslib.ConsolidateSummaryItems(handler.consolidatedSummary, update)
+	assert.Equal(t, expectedSummary, summaryRecord)
+}
+
+func TestHandleRequestDefer(t *testing.T) {
+	handler := &Handler{
+		recordChan: make(chan *service.Record, 1),
+	}
+
+	deferRequest := &service.Request{
+		RequestType: &service.Request_Defer{
+			Defer: &service.DeferRequest{State: service.DeferRequest_FLUSH_PARTIAL_HISTORY},
+		},
+	}
+	record := &service.Record{
+		RecordType: &service.Record_Request{Request: deferRequest},
+	}
+	handler.historyRecord = &service.HistoryRecord{}
+
+	handler.handleRequest(record)
+
+	receivedRecord := <-handler.recordChan
+	assert.NotNil(t, receivedRecord)
+	assert.Nil(t, handler.historyRecord)
+}
+
+func TestHandleGetSummary(t *testing.T) {
+	handler := &Handler{
+		consolidatedSummary: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	response := &service.Response{}
+	handler.handleGetSummary(nil, response)
+
+	expectedItems := []*service.SummaryItem{
+		{Key: "key1", ValueJson: "value1"},
+		{Key: "key2", ValueJson: "value2"},
+	}
+
+	assert.Equal(t, expectedItems, response.GetGetSummaryResponse().Item)
+}
+
+func TestFlushHistory(t *testing.T) {
+	handler := &Handler{
+		startTime:  1000,
+		recordChan: make(chan *service.Record, 2),
+	}
+
+	history := &service.HistoryRecord{
+		Item: []*service.HistoryItem{
+			{Key: "_timestamp", ValueJson: "2000"},
+		},
+		Step: &service.HistoryStep{Num: 1},
+	}
+
+	handler.flushHistory(history)
+
+	summaryRecord := <-handler.recordChan
+	historyRecord := <-handler.recordChan
+
+	assert.Equal(t, "_runtime", summaryRecord.GetSummary().Update[0].Key)
+	assert.Equal(t, "1.000000", summaryRecord.GetSummary().Update[0].ValueJson)
+	assert.Equal(t, "_step", summaryRecord.GetSummary().Update[1].Key)
+	assert.Equal(t, "1", summaryRecord.GetSummary().Update[1].ValueJson)
+	assert.Equal(t, history, historyRecord.GetHistory())
+}
+
+func TestHandlePartialHistory(t *testing.T) {
+	handler := &Handler{
+		historyRecord: &service.HistoryRecord{Step: &service.HistoryStep{Num: 1}},
+	}
+
+	request := &service.PartialHistoryRequest{
+		Step:   &service.HistoryStep{Num: 2},
+		Item:   []*service.HistoryItem{{Key: "key1", ValueJson: "value1"}},
+		Action: &service.HistoryAction{Flush: true},
+	}
+
+	handler.handlePartialHistory(nil, request)
+
+	assert.Equal(t, int32(3), handler.historyRecord.Step.Num)
 }
